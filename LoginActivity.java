020c6de855f479bcb2277e2d944de95f107dcfaf
kelvinapp <47050577+kelvinapp@users.noreply.github.com>
2019-06-15 09:35:50 +0800

    Add files via upload

---
+++ LoginActivity.java
@@ -0,0 +1,66 @@
+package com.example.atm;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edUserid;
+    private EditText edPasswd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUserid = findViewById(R.id.userid);
+        edPasswd = findViewById(R.id.passwd);
+
+
+    }
+
+    public void login(View vie) {
+        String userid = edUserid.getText().toString();
+        final String passwd = edPasswd.getText().toString();
+       FirebaseDatabase.getInstance().getReference("users").child(userid).child("password")
+               .addListenerForSingleValueEvent(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                       String pw = (String) dataSnapshot.getValue();
+                       if (pw.equals(passwd)){
+                           setResult(RESULT_OK);
+                           finish();
+                       }else {
+                           new AlertDialog.Builder(LoginActivity.this)
+                                   .setTitle("登入结果")
+                                   .setMessage("登入失敗")
+                                   .setPositiveButton("OK",null)
+                                   .show();
+                       }
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                   }
+               });
+//        if ("jack".equals(userid) && "1234".equals(passwd)) {
+//
+//        }
+    }
+
+    public void quit(View view) {
+    }
+}
